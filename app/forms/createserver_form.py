7a236ae44a105f1aec20a91f478cb792b6db3cb2
oscar <oscarvac74@gmail.com>
2023-07-31 17:07:12 -0500

    ServerForm

--- app/forms/createserver_form.py
+++ app/forms/createserver_form.py
@@ -1,5 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired,  ValidationError, Length
-from ..models.user import User
-from ..api
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired,  ValidationError
+from ..models.server import Server
+from flask_wtf.file import  FileField, FileAllowed
+from ..api.AWS_helpers import ALLOWED_EXTENSIONS
+
+def server_name_exists(form, field):
+    # Checking if Servername is already in usee
+    name = field.data
+    server = Server.query.filter(Server.name == name).first()
+    if server:
+        raise ValidationError('Servername is already in use.')
+
+class CreateServerForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(), server_name_exists])
+    public = BooleanField("Public")
+    server_image = FileField("Server Image", validators=[ FileAllowed(list(ALLOWED_EXTENSIONS))])
+    banner_image = FileField("Banner Image", validators=[ FileAllowed(list(ALLOWED_EXTENSIONS))])
+    description = StringField("Description")
+    submit = SubmitField("Submit")
