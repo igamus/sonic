18e193483ce20b80ea633f9f474ed28864ebbea6
IceLordUlmo <torgall22@gmail.com>
2023-07-31 12:30:50 -0500

    server route

---
+++ app/api/server_routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, jsonify, session, request
+from app.models import User, Server, db
+from flask_login import current_user, login_required
+
+server_routes = Blueprint('server', __name__)
+
+@server_routes.route('/current')
+@login_required
+def current_servers():
+    """
+    Return a list of the servers current user is a member of.
+    """
+
+    serverList = {
+        current_user.servers
+    }
+
+    return serverList
