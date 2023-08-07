70106eab9b5ce2e398d0ed71acc5099a9f9df400
Isaac Gamus <igamus@gmail.com>
2023-08-07 00:55:48 -0400

    getting stingy with sockets

--- react-app/src/components/ReactionSelector/index.js
+++ react-app/src/components/ReactionSelector/index.js
@@ -9,9 +9,13 @@ import { useDispatch } from "react-redux";
 
 let socket;
 
-function ReactionSelector({ channelId, userId, messageId }) {
+function ReactionSelector({ channelId, userId, message }) {
     const { closeModal } = useModal();
     const dispatch = useDispatch();
+
+    console.log("message", message);
+    const userReactions = message.reactions.filter(reaction => reaction.ownerId === userId).map(reaction => reaction.emoji)
+    console.log("userReactions:", userReactions)
 
     useEffect(() => {
         socket = io();
@@ -27,16 +31,23 @@ function ReactionSelector({ channelId, userId, messageId }) {
 
     const handleClick = (e, emoji) => {
         e.preventDefault();
-        console.log("emit an emoji", emoji);
-        console.log("for message:", messageId)
-        socket.emit("react", {owner_id: userId, message_id: messageId, emoji: emoji})
+        console.log("for message:", message.id)
+        if (userReactions.includes(emoji)) {
+            console.log("remove an emoji", emoji);
+            socket.emit("delete_reaction", {"message_id": parseInt(message.id), "owner_id": parseInt(userId), emoji: emoji})
+        } else {
+            console.log("emit an emoji", emoji);
+            socket.emit("react", {owner_id: parseInt(userId), message_id: parseInt(message.id), emoji: emoji})
+        }
         closeModal();
     };
     return (
         <div className="panel-selector">
             {emojiList.map(emoji => {
+                let className = "panel-reaction";
+                if (userReactions.includes(emoji)) className += " user-owns";
                 const val = "&#x" + emoji + ";"
-                return (<span className="panel-reaction" onClick={(e) => handleClick(e, emoji)} value={`${emoji}`} dangerouslySetInnerHTML={{__html: sanitizeHtml(val)}} />)
+                return (<span className={className} onClick={(e) => handleClick(e, emoji)} value={`${emoji}`} dangerouslySetInnerHTML={{__html: sanitizeHtml(val)}} />)
             })}
         </div>
     );
